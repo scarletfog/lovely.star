683861c0d2e01939ef2d8c5434293f3edbfa89f5
Anna Cieslik <scarletfog@gmail.com>
2023-10-10 13:12:29 +0200

    🔧 add basic project configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve('./build'),
+        filename: './static/js/bundle-[chunkhash].js',
+        chunkFilename: './static/js/[chunkhash].js'
+      },
+    target: "web",
+    devServer: {
+        port: "8080",
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
